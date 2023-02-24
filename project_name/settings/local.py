02ae47f84729d88b3c0d70615b5100b8b0c6d9a9
Benjamin Liet <benjamin.liet.dev@gmail.com>
2023-02-24 11:16:06 +0100

    ✨feature: Created custom django startproject template

---
+++ project_name/settings/local.py
@@ -0,0 +1,31 @@
+import os
+from .base import BASE_DIR, env
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{{ project_name }}_development',
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
+}
+
+SHARE_URL = "http://127.0.0.1:8000"
+
+# Static assets
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'static_dirs'),
+)
+# User uploads
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
---
+++ project_name/settings/production.py
@@ -0,0 +1,6 @@
+DEBUG = False
+ALLOWED_HOSTS = []
+
+# Google Cloud DB
+
+# Google Cloud Storage
